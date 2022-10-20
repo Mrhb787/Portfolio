375aefaf9b625872a39ac12db031b8cc87328ade
Abhishek Singh <56078689+Mrhb787@users.noreply.github.com>
2022-10-21 05:24:20 +0530

    Fixatures

---
+++ src/Components/App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./App.css";
+
+// Components
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
