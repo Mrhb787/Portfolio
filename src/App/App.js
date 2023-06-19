87ad0eff5f8c3ea9fd78073cfce9d074e694ae27
Bookshlf <Bookshlf@ACKO2039.local>
2023-06-19 14:24:57 +0530

    Portfolio re-initialized

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+
+// Components
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -9,23 +9,20 @@ import "./index.css";
 import {BrowserRouter} from "react-router-dom";
 
 // component
-import App from "./Components/App/App";
+import App from "./App/App";
 import {SnackbarProvider} from "notistack";
 import {HelmetProvider} from "react-helmet-async";
-import {CurrentUserProvider} from "./Context/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CurrentUserProvider>
-        <SnackbarProvider maxSnack={3}>
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
-        </SnackbarProvider>
-      </CurrentUserProvider>
+      <SnackbarProvider maxSnack={3}>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </SnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
